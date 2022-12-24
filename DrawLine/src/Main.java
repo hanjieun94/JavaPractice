4c14af74a495d5189c7d9931bc148cc48f59fde7
hanjieun94 <120148678+hanjieun94@users.noreply.github.com>
2022-12-24 23:56:42 +0900

    Add files via upload

---
+++ DrawLine/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Canvas canvas = new Canvas();
+
+        //drawLine()함수 호출
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = 13;
+        int y2 = 13;
+
+        canvas.drawLineOnCanvas(canvas,x1, x2, y1, y2);
+    }
+
+}
---
+++ DrawLine/src/Structured.java
@@ -0,0 +1,65 @@
+public class Structured {
+
+    public static void main(String[] args) {
+        //#10
+
+        //1. canvas라는 이름으로 문자 20x20개를 저장할 수 있는 배열 객체를 생성한다.
+        char[][] canvas = new char[20][20];
+
+
+        //2. canvas 배열을 다음처럼 ‘┼’ 문자로 채우는 코드를 작성하시오.
+        {
+            setCanvas(canvas);
+            printCanvas(canvas);
+            System.out.println("board 초기화 완료");
+        }
+
+
+        //3. ‘○’ 문자를 이용해서 canvas에 선을 그리는 함수를 작성하시오.
+        {
+            int x1 = 2;
+            int x2 = 13;
+            int y1 = 2;
+            int y2 = 13;
+
+            //drawLineOnCanvas함수 호출
+            drawLineOnCanvas(canvas, x1, x2, y1, y2);
+        }
+
+    }
+
+    //drawLineOnCanvas함수
+    static void drawLineOnCanvas(char[][]canvas, int x1, int x2, int y1, int y2) {
+        setCanvas(canvas);
+        for(int y=0; y<20; y++) {
+            for(int x=0; x<20; x++) {
+                if((y==x) && ((x>=x1&&x<x2)||(y>=y1&&y<=y2)))
+                    canvas[y][x] = '○';
+            }
+        }
+        //printCanvas함수 호출
+        printCanvas(canvas);
+
+    }
+
+
+    //setCanvas함수
+    static void setCanvas(char[][] canvas) {
+        for(int y=0; y<20; y++)
+            for(int x=0; x<20; x++)
+                canvas[y][x] = '┼';
+    }
+
+
+    //printCanvas함수
+    static void printCanvas(char[][] canvas) {
+        for(int y=0; y<20; y++) {
+            for(int x=0; x<20; x++)
+                System.out.print(canvas[y][x]);
+            System.out.println();
+        }
+
+    }
+
+
+}
